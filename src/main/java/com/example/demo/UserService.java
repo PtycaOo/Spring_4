3b5094e016362cc141d5eac588397815fc0e680d
DenKu <Denkuzia5155@gmail.com>
2024-10-19 17:29:30 +0300

    #2

--- src/main/java/com/example/demo/UserService.java
+++ src/main/java/com/example/demo/UserService.java
@@ -1,11 +1,13 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 public class UserService {
 
+    @Autowired
     private UserRepository userRepository;
 
     public List<User> getAllUsers(){
